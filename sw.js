be8648cce0862c3d8a9a3e5099a2b8fca1cb5f58
Roy Kolak <roy@curiosity.com>
2017-09-15 14:33:13 -0500

    initial import

---
+++ sw.js
@@ -0,0 +1,36 @@
+var CACHE_NAME = 'version_05'
+var URLS = [               // Add URL you want to cache in this list.
+  '/',                     // If you have separate JS/CSS files,
+  '/index.html'            // add path to those files here
+]
+
+// Respond with cached resources
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (request) {
+      return request || fetch(event.request)
+    })
+  )
+})
+
+// Cache resources
+self.addEventListener('install', function (event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      return cache.addAll(URLS)
+    })
+  )
+})
+
+// Delete outdated caches
+self.addEventListener('activate', function (event) {
+  event.waitUntil(
+    caches.keys().then(function (keyList) {
+      return Promise.all(keyList.map(function (key, i) {
+        if (key !== CACHE_NAME) {
+          return caches.delete(keyList[i])
+        }
+      }))
+    })
+  )
+})
